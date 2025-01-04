ac5c87cacc4f84d88e3c44df1737a4a325e2897a
Sam Kikoma <kikomasamson@gmail.com>
2025-01-04 23:08:30 +0100

    Feat : To-Do status can now be updated

--- src/index.ts
+++ src/index.ts
@@ -31,10 +31,6 @@ class Todo {
 	taskName: string;
 	dueDate: string;
 	taskDone: boolean;
-
-	toggleDone = () => {
-		this.taskDone = !this.taskDone;
-	};
 }
 
 const createTodo = () => {
@@ -62,13 +58,32 @@ const listTodos = () => {
 	});
 };
 
-program.name("CLI To-Do").description("Simple CLI To-Do List").version("1.0.0");
+const deleteTodo = () => {
+	loadData();
+	let deletedId = parseInt(uPrompt("Please enter index of the to-do you want to delete : "));
+	const newTodos = todos.filter((todo, index) => index !== deletedId - 1);
+	const newData = JSON.stringify(newTodos, null, 2);
+	fs.writeFileSync("data.json", newData);
+};
 
+const updateTodo = () => {
+	loadData();
+	const updateId = parseInt(uPrompt("Index of to-do you want to update : "));
+	const todo = todos[updateId - 1];
+	todo.taskDone = !todo.taskDone;
+	saveData();
+	console.log(`To-do "${todo.taskName}" has been marked as ${todo.taskDone ? "completed ✔️" : "not completed ❌"}.`);
+};
+
+program.name("CLI To-Do").description("Simple CLI To-Do List").version("1.0.0");
 program.command("create").description("Creates a to-do").action(createTodo);
 program.command("list").description("Lists all to-dos").action(listTodos);
+program.command("delete").description("Delete a specific todo").action(deleteTodo);
+program.command("update").description("Update the status of a to-do").action(updateTodo);
 
 program.parse(process.argv);
 // Improvements //
-// Read from data.json then push to array
-// Save todos to json file
-// Add delete and complete methods
+// Read from data.json then push to array - Done
+// Save todos to json file - Done
+// Delete to-do - Done
+// Updating to-do
