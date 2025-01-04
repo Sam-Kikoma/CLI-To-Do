55c9f55e9e52212ce33969d3232765299895ab92
Sam Kikoma <kikomasamson@gmail.com>
2025-01-04 23:09:17 +0100

    Dependcies

--- dist/index.js
+++ dist/index.js
@@ -20,9 +20,6 @@ const saveData = () => {
 loadData();
 class Todo {
     constructor(taskName, date, taskDone) {
-        this.toggleDone = () => {
-            this.taskDone = !this.taskDone;
-        };
         this.taskName = taskName;
         this.dueDate = date;
         this.taskDone = false;
@@ -51,12 +48,30 @@ const listTodos = () => {
         console.log(`${index + 1}. ${todo.taskName} (Due: ${todo.dueDate}) ${todo.taskDone ? "✔️" : "❌"}`);
     });
 };
+const deleteTodo = () => {
+    loadData();
+    let deletedId = parseInt(uPrompt("Please enter index of the to-do you want to delete : "));
+    const newTodos = todos.filter((todo, index) => index !== deletedId - 1);
+    const newData = JSON.stringify(newTodos, null, 2);
+    fs.writeFileSync("data.json", newData);
+};
+const updateTodo = () => {
+    loadData();
+    const updateId = parseInt(uPrompt("Index of to-do you want to update : "));
+    const todo = todos[updateId - 1];
+    todo.taskDone = !todo.taskDone;
+    saveData();
+    console.log(`To-do "${todo.taskName}" has been marked as ${todo.taskDone ? "completed ✔️" : "not completed ❌"}.`);
+};
 program.name("CLI To-Do").description("Simple CLI To-Do List").version("1.0.0");
 program.command("create").description("Creates a to-do").action(createTodo);
 program.command("list").description("Lists all to-dos").action(listTodos);
+program.command("delete").description("Delete a specific todo").action(deleteTodo);
+program.command("update").description("Update the status of a to-do").action(updateTodo);
 program.parse(process.argv);
 // Improvements //
-// Read from data.json then push to array
-// Save todos to json file
-// Add delete and complete methods
+// Read from data.json then push to array - Done
+// Save todos to json file - Done
+// Delete to-do - Done
+// Updating to-do
 //# sourceMappingURL=index.js.map
